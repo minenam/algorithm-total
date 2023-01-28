b4833254f12d46cf3ced02dc8bb4c69b714501d0
Hyemin Nam <35288895+minenam@users.noreply.github.com>
2023-01-28 15:26:26 +0900

    [level 1] Title: 최소직사각형, Time: 2.23 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,26 +1,46 @@
 function solution(sizes) {
-    let maxValue = 0;
-    let minValue = 0;
+    let i = 0;
     
-    for (let size of sizes) {
-        let weight = size[0];
-        let height = size[1];
+    // 한 명함 내에서 길이
+    let smallerSize = 0;
+    let largerSize = 0;
+    
+    // 전체 명함의 길이
+    let lastSmallest = 0;
+    let lastLargest = 0;
+    
+    while (i < sizes.length) {
+        // 한 명함의 길이 (가로, 세로)
+        let weight = sizes[i][0];
+        let height = sizes[i][1];
         
+        // 가로가 더 길 때
         if (weight > height) {
-            if (weight > maxValue) {
-                maxValue = weight
+            largerSize = weight;
+            smallerSize = height;
+            // 이전까지의 누적 큰 값과 비교하기
+            if (weight > lastLargest) {
+                lastLargest = weight;
             }
-            if (height > minValue) {
-                minValue = height
+            // 이전까지의 누적 작은 값과 비교하기
+            if (height > lastSmallest) {
+                lastSmallest = height;
             }
+        // 세로가 더 길 때
         } else {
-            if (height > maxValue) {
-                maxValue = height
+            largerSize = height;
+            smallerSize = weight;
+            // 이전까지의 누적 큰 값과 비교하기
+            if (height > lastLargest) {
+                lastLargest = height;
             }
-            if (weight > minValue) {
-                minValue = weight
+            // 이전까지의 누적 작은 값과 비교하기
+            if (weight > lastSmallest) {
+                lastSmallest = weight;
             }
         }
+        i++;
     }
-    return maxValue * minValue;
+
+    return lastLargest * lastSmallest; // 최종 길이끼리 곲
 }

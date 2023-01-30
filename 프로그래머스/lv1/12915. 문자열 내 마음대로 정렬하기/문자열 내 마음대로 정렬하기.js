034d931093343b07b572c17b06b4586162b6839e
Hyemin Nam <35288895+minenam@users.noreply.github.com>
2023-01-30 13:49:04 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 7.33 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,16 +1,22 @@
 function solution(strings, n) {
-    let answer = [];
-    let newStrings = []; // [ "a", "e", "u"];
+    /*
+    - 반복문으로 n 인덱스 글자를 찾아 {n번째 글자: 해당 글자 인덱스} 해시테이블 만들기
+    - 해시테이블의 키를 배열로 만들고 정렬하기
+    - 반복문으로 원래 단어 배열에 해당하는 인덱스와, 해시테이블의 인덱스 값을 비교하여 새로운 배열 반환
+    */
+    let answer = [...strings];
     
-    for (let i = 0; i < strings.length; i++) {
-        let nIndex = strings[i][n];
-        strings[i] = nIndex+strings[i];
-    }
-    strings.sort();
-    
-    for (let i = 0; i < strings.length; i++) {
-        answer.push(strings[i].slice(1,))
-    }
+    answer.sort((a, b) => {
+        console.log(a[n], b[n]);
+        
+        // 문자열 n번째 인덱스 글자를 기준으로 오름차순 정렬
+        if (a[n] > b[n]) return 1;
+        if (a[n] < b[n]) return -1;
+        
+        // 동일한 인덱스 글자를 가진 단어들끼리 사전순 정렬
+        if (a > b) return 1;
+        if (a < b) return -1;
+    });
     
     return answer;
 }

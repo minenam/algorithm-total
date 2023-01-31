98520ec458a394a555d29a26343387841cd4d84b
Hyemin Nam <35288895+minenam@users.noreply.github.com>
2023-01-31 11:19:31 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,13 +1,32 @@
 function solution(s) {
-    const RULE = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-
-    let answer = s;
+    const RULE = {
+        'zero': 0,
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+    };
     
-    // 정규표현식 적용
-    for (let i = 0; i < RULE.length; i++) {
-        let regex = new RegExp(RULE[i], 'g');
-        answer = answer.replace(regex, i);
+    let answer = '';
+    let temp = '';
+    
+    for (let i = 0; i < s.length; i++) {
+        // 숫자인 경우
+        if (!isNaN(s[i])) {
+            answer += s[i];
+        } else {
+            // 문자인 경우
+            temp += s[i];
+            if (Object.keys(RULE).includes(temp)) {
+                answer += RULE[temp];
+                temp = '';
+            }
+        }   
     }
-    
     return parseInt(answer);
 }

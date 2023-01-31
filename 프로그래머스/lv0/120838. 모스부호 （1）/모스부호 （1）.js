cffb1c92be6840c6741739b7b73a4a18d23f0425
Hyemin Nam <35288895+minenam@users.noreply.github.com>
2023-01-31 10:08:39 +0900

    [level 0] Title: 모스부호 (1), Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,16 @@
+function solution(letter) {
+    let answer = '';
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    };
+    
+    let inputLetter = letter.split(' ');
+    
+    inputLetter.forEach((v) => answer += morse[v] );
+    
+    return answer;
+}

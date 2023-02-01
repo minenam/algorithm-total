8a88e318671083c5999fd7679083a982d6aba6f9
Hyemin Nam <35288895+minenam@users.noreply.github.com>
2023-02-01 17:44:37 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,19 +1,29 @@
+// function solution(left, right) {
+//     let answer = 0;
+    
+//     // 약수 개수 구하는 함수
+//     const countDivisor = (num) => {
+//         let count = 0;
+//         for (let i = 1; i <= num; i++) {
+//             if (num % i === 0) count+= 1;
+//         }
+//         return count;
+//     };
+    
+//     // 약수 개수의 짝/홀수로 계산
+//     for (let i = left; i < right+1; i++) {
+//         if (countDivisor(i) % 2 === 0) answer += i;
+//         else answer -= i;
+//     }
+    
+//     return answer;
+// }
+
 function solution(left, right) {
-    let answer = 0;
+    var answer = 0;
     
-    // 약수 개수 구하는 함수
-    const countDivisor = (num) => {
-        let count = 0;
-        for (let i = 1; i <= num; i++) {
-            if (num % i === 0) count+= 1;
-        }
-        return count;
-    };
-    
-    // 약수 개수의 짝/홀수로 계산
-    for (let i = left; i < right+1; i++) {
-        if (countDivisor(i) % 2 === 0) answer += i;
-        else answer -= i;
+    for(let i=left; i<=right; i++){
+        Math.sqrt(i)%1===0 ? answer-=i : answer+=i;
     }
     
     return answer;

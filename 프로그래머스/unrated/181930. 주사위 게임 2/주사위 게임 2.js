7f99f5fe303027b353be7bb511c40b9be602a6bb
Hyemin Nam <35288895+minenam@users.noreply.github.com>
2023-05-16 10:01:48 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,13 @@
+function solution(a, b, c) {
+    let answer = 0;
+    
+    if (a !== b && b !== c && a !== c) {
+        answer = a + b + c;
+    } else if ( a === b && b === c && a == c) {
+        answer = (a + b + c) * (a**2 + b**2 + c**2 ) * (a**3 + b**3 + c**3 );
+    } else {
+        answer = (a + b + c) * (a**2 + b**2 + c**2 );
+    }
+    
+    return answer;
+}

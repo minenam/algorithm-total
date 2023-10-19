0f055e3291afd272ff42493635900bd1442e95e3
Hyemin Nam <35288895+minenam@users.noreply.github.com>
2023-10-20 02:13:17 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,8 +1,4 @@
 function solution(num_list) {
-    const result = [];
-    num_list.forEach((value, index) => { 
-        if (value < 0) {
-            result.push(index);
-        }});
-    return result.length ? result[0] : -1;
+    const result = num_list.findIndex(value => value < 0 );
+    return result ?? -1;
 }

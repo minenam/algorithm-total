de4eb62418e206bdd46a776b6155c0e6e64642c5
Heyden Nam <35288895+minenam@users.noreply.github.com>
2025-01-17 01:04:53 +0900

    [Bronze IV] Title: 모음의 개수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.py
@@ -0,0 +1,11 @@
+strings = input()
+
+answer = 0
+
+answer += strings.count('a')
+answer += strings.count('e')
+answer += strings.count('i')
+answer += strings.count('o')
+answer += strings.count('u')
+
+print(answer)
